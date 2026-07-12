6b4855c82ddd8697bcb84283f26d81bbe0ea97fd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "logic_app_integration_account_batch_configurations" {
-  description = "All logic_app_integration_account_batch_configuration resources"
-  value       = azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations
-}
 output "logic_app_integration_account_batch_configurations_batch_group_name" {
-  description = "List of batch_group_name values across all logic_app_integration_account_batch_configurations"
-  value       = [for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : v.batch_group_name]
+  description = "Map of batch_group_name values across all logic_app_integration_account_batch_configurations, keyed the same as var.logic_app_integration_account_batch_configurations"
+  value       = { for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : k => v.batch_group_name }
 }
 output "logic_app_integration_account_batch_configurations_integration_account_name" {
-  description = "List of integration_account_name values across all logic_app_integration_account_batch_configurations"
-  value       = [for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : v.integration_account_name]
+  description = "Map of integration_account_name values across all logic_app_integration_account_batch_configurations, keyed the same as var.logic_app_integration_account_batch_configurations"
+  value       = { for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : k => v.integration_account_name }
 }
 output "logic_app_integration_account_batch_configurations_metadata" {
-  description = "List of metadata values across all logic_app_integration_account_batch_configurations"
-  value       = [for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : v.metadata]
+  description = "Map of metadata values across all logic_app_integration_account_batch_configurations, keyed the same as var.logic_app_integration_account_batch_configurations"
+  value       = { for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : k => v.metadata }
 }
 output "logic_app_integration_account_batch_configurations_name" {
-  description = "List of name values across all logic_app_integration_account_batch_configurations"
-  value       = [for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : v.name]
+  description = "Map of name values across all logic_app_integration_account_batch_configurations, keyed the same as var.logic_app_integration_account_batch_configurations"
+  value       = { for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : k => v.name }
 }
 output "logic_app_integration_account_batch_configurations_release_criteria" {
-  description = "List of release_criteria values across all logic_app_integration_account_batch_configurations"
-  value       = [for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : v.release_criteria]
+  description = "Map of release_criteria values across all logic_app_integration_account_batch_configurations, keyed the same as var.logic_app_integration_account_batch_configurations"
+  value       = { for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : k => v.release_criteria }
 }
 output "logic_app_integration_account_batch_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_integration_account_batch_configurations"
-  value       = [for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_integration_account_batch_configurations, keyed the same as var.logic_app_integration_account_batch_configurations"
+  value       = { for k, v in azurerm_logic_app_integration_account_batch_configuration.logic_app_integration_account_batch_configurations : k => v.resource_group_name }
 }
 
